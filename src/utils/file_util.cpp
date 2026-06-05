a58a70055a505a102188f205cd998971a618ba19
Ptah <u20mk2d7@gmail.com>
2026-06-05 08:45:42 +0700

    Update file_util.cpp

---
+++ src/utils/file_util.cpp
@@ -0,0 +1,13 @@
+#include "utils/file_util.hpp"
+
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <cstdlib>
+#include <filesystem>
+
+namespace bluestone {
+
+std::string FileUtil::read_key_file(std::string_view filepath) {}
+
+} // namespace bluestone
